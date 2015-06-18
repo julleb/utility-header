3ca46cfa0ddfaf1f4648a102547d32329eb16054
dacechavez <dacechavez@gmail.com>
2015-06-18 16:18:36 +0200

    New namespace to avoid collisions, added two useful functions and some documentation.

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,8 +5,40 @@
 #include <string>
 #include <vector>
 
-TEST_CASE( "Splitting strings with spaces", "[strings]" ) {
+TEST_CASE( "Splitting strings with spaces", "[strings/tokenize]" ) {
     std::string test_string1 = "Hello World!";
+    std::string test_string2 = "";
+    std::string test_string3 = "1";
     std::vector<std::string> should_be1 = {"Hello", "World!"};
-    REQUIRE( split_on_spaces(test_string1) == should_be1 );
+    std::vector<std::string> should_be2 = {""};
+    std::vector<std::string> should_be3 = {"1"};
+    REQUIRE( utils::split_on_spaces(test_string1) == should_be1 );
+    REQUIRE( utils::split_on_spaces(test_string2) == should_be2 );
+    REQUIRE( utils::split_on_spaces(test_string3) == should_be3 );
+
 }
+
+TEST_CASE( "Changing strings to lowercase", "[strings/to_lower]" ) {
+    std::string test_string1 = "Hello World!";
+    std::string test_string2 = "";
+    std::string test_string3 = "11A11a11";
+    utils::to_lower(test_string1);
+    utils::to_lower(test_string2);
+    utils::to_lower(test_string3);
+    REQUIRE( test_string1 == "hello world!" );
+    REQUIRE( test_string2 == "" );
+    REQUIRE( test_string3 == "11a11a11" );
+}
+
+TEST_CASE( "Changing strings to uppercase", "[strings/to_upper]" ) {
+    std::string test_string1 = "Hello World!";
+    std::string test_string2 = "";
+    std::string test_string3 = "11A11a11";
+    utils::to_upper(test_string1);
+    utils::to_upper(test_string2);
+    utils::to_upper(test_string3);
+    REQUIRE( test_string1 == "HELLO WORLD!" );
+    REQUIRE( test_string2 == "" );
+    REQUIRE( test_string3 == "11A11A11" );
+}
+
--- utility-header.hpp
+++ utility-header.hpp
@@ -3,44 +3,69 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
-std::vector<std::string> split_on_spaces(const std::string& str);
-std::vector<std::string> tokenize(const std::string& str, const std::string& delim);
+namespace utils {
 
-/* TODO documentation*/
-std::vector<std::string> split_on_spaces(const std::string& str)
-{
-    return tokenize(str, std::string(" "));
-}
+    // Splits a string on spaces and returns a vector:
+    // "Hello World !" => ["Hello", "World", "!"]
+    std::vector<std::string> split_on_spaces(const std::string& s);
 
-/* TODO documentation*/
-std::vector<std::string> tokenize(const std::string& str, const std::string& delim)
-{
-    // result vector
-    std::vector<std::string> tokens;
+    // Splits a string on delimeter and returns a vector:
+    // "the_cat-is_black" => ["the", "cat-is", "black"]
+    // if the delimeter is an underscore.
+    std::vector<std::string> tokenize(const std::string& s, const std::string& delim);
 
-    // we'll start from str[0] and look for a delimeter
-    size_t pos = 0;
-    size_t delim_location = str.find_first_of(delim, pos);
+    // Modifies your string and converts it to lowercase.
+    void to_lower(std::string& s);
 
-    // no delimiters found, cant tokenize
-    if(delim_location == std::string::npos) {
-        tokens.push_back(str);
-    } else {
-        while(pos != std::string::npos) {
-            // push back word without delimiter
-            tokens.push_back(str.substr(pos, delim_location - pos));
-            if(delim_location == std::string::npos) {
-                // end of string
-                pos = delim_location;
-            } else {
-                // next char, skip the delimiter
-                pos = delim_location + 1;
-            }
-            delim_location = str.find_first_of(delim, pos);
-        }
+    // Modifies your string and converts it to uppercase.
+    void to_upper(std::string& s);
+
+    ////////////////////
+    // Implementation //
+    ////////////////////
+
+    std::vector<std::string> split_on_spaces(const std::string& s) {
+        return tokenize(s, std::string(" "));
     }
-    return tokens;
-}
 
+    std::vector<std::string>
+    tokenize(const std::string& s, const std::string& delim) {
+        // put substrings in this result vector as soon as you find a delimeter
+        std::vector<std::string> tokens;
+
+        // we'll start from s[0] and look for a delimeter
+        size_t pos = 0;
+        size_t delim_location = s.find_first_of(delim, pos);
+
+        // no delimiters found, cant tokenize
+        if(delim_location == std::string::npos) {
+            tokens.push_back(s);
+        } else {
+            while(pos != std::string::npos) {
+                // push back word without delimiter
+                tokens.push_back(s.substr(pos, delim_location - pos));
+                if(delim_location == std::string::npos) {
+                    // end of string
+                    pos = delim_location;
+                } else {
+                    // next char, skip the delimiter
+                    pos = delim_location + 1;
+                }
+                delim_location = s.find_first_of(delim, pos);
+            }
+        }
+        return tokens;
+    }
+
+    void to_lower(std::string& s) {
+        std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+    }
+
+    void to_upper(std::string& s) {
+        std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+    }
+
+}
 #endif
